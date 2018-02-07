6b83991f1f8cd8de7484bb523d8a0fd39e63da1f
David Lawton <mailsmeg@gmail.com>
2018-02-07 09:42:56 +0000

    Create LamdaContainer.php

---
+++ LamdaContainer.php
@@ -0,0 +1,30 @@
+<?php
+
+// Lightweight class to register lamda functions to a global container. 
+// Lamda functions could typically be used to construct objects complete with dependencies ready for DI
+
+class LambdaContainer
+{
+    private static $registry = array();
+ 
+    public static function register($name, $lambda)
+    {
+        if (!class_exists($name) || !is_callable($lambda)) {
+            throw new \InvalidArgumentException('Invalid arguments in call to Container::register');
+        }
+        self::$registry[$name] = $lambda;
+    }
+ 
+    public static function get($name)
+    {
+        if (!class_exists($name)) {
+            throw new \InvalidArgumentException('Invalid class in call to Container::get');
+        }
+ 
+        if (!array_key_exists($name, self::$registry)) {
+            throw new \Exception('Class has not been registered');
+        }
+        $lambda = self::$registry[$name];
+        return $lambda();
+    }
+}
